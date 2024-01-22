93810e953ebfee916768f17be3ccadbc0ca1612a
Elisee-des <esabidani@gmail.com>
2024-01-22 13:44:06 +0000

    Integration template public

---
+++ src/main/java/com/admin_user/controller/publics/PublicController.java
@@ -0,0 +1,13 @@
+package com.admin_user.controller.publics;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class PublicController {
+	
+	@GetMapping("/")
+	public String index()
+	{
+		return "index";
+	}
+
+}
